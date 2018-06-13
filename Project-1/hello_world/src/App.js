76d2d042cfafded7db66e63cc892f26169e798d9
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-06-13 19:44:24 +0530

    Basic Components added

--- Project-1/hello_world/src/App.js
+++ Project-1/hello_world/src/App.js
@@ -6,16 +6,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Header/>
+          <Intro/>
       </div>
     );
   }
+}
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+            </div>
+
+        );
+    }
+}
+
+class Intro extends Component{
+    render(){
+        return(
+            <div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+
+        );
+    }
 }
 
 export default App;
