fcf9fed8ec429f00b9da35c1a2f4c705a8cdbf4d
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-06-19 01:10:14 +0530

    Constructor added

--- Project-2/component-basic/src/App.js
+++ Project-2/component-basic/src/App.js
@@ -3,17 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor
+    constructor(props){
+        super(props);
+        this.yourname = "Toby";
+        this.state={}; //Default values are defined when th props are loaded
+        
+    }
 
     sayHello(name){
-        return "Hello"+name;
+        return "Hello "+name;
     }
   render() {
       const name="Abhirupa";
     return (
       <div className="App">
         <h2>I am a React Developer: {name}</h2>
-          {this.sayHello(" Abhi")}
+          {this.sayHello(this.yourname)}
       </div>
     );
   }
