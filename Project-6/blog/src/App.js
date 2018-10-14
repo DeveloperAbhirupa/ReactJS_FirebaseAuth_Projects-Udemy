b19db0c2d8d58da9918b488a9451048c8df5ad16
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-14 17:34:08 +0530

    All components made

--- Project-6/blog/src/App.js
+++ Project-6/blog/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar.js'
-//import Headr from './Headr'
+import Headr from './Headr.js'
 import Containr from './Containr.js'
 import Footer from './Footer.js'
 class App extends Component {
@@ -11,25 +11,8 @@ class App extends Component {
       <div>
 
         <Navbar/>
-
-
-          <header className="masthead headerStyle" >s
-            <div className="overlay"></div>
-            <div className="container">
-              <div className="row">
-              <div className="col-lg-8 col-md-10 mx-auto">
-                  <div className="site-heading">
-                    <h1>Clean Blog</h1>
-                    <span className="subheading">A Blog Theme by Start Bootstrap</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </header>
-
-          <Containr/>
-
-
+        <Headr/>
+        <Containr/>
         <hr/>
 
       <Footer/>
